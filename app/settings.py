568b62616ff7201aa233454a81fe843bb1a37653
Mohammed <78994175+branson404@users.noreply.github.com>
2025-08-25 18:19:35 +0530

    fix: allowed all hosts

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i&k^i-ys5=j&hmb(26)z==fc9h#x1!!p9z^e6^%i-%%x5uu4l_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,3 +124,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
